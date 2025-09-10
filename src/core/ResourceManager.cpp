472109fa3f30a4057d4e8676cd452f1257cfbd87
Ollie <olliemoss321@gmail.com>
2025-09-10 20:59:30 +1200

    Trying compile time shader path

--- src/core/ResourceManager.cpp
+++ src/core/ResourceManager.cpp
@@ -10,34 +10,41 @@
 #include "ft2build.h"
 #include FT_FREETYPE_H
 
-const Shader &ResourceManager::LoadShader(const char *vShaderFile, const char *fShaderFile, std::string name) {
+#define SHADER_PATH
+
+const Shader &ResourceManager::LoadShader(const char *vShaderFile, const char *fShaderFile, std::string name)
+{
     Shader shader;
     shader.Compile(vShaderFile, fShaderFile);
     m_Shaders[name] = shader;
     return m_Shaders[name];
 }
-bool ResourceManager::Init() {
+bool ResourceManager::Init()
+{
     stbi_set_flip_vertically_on_load(true);
 
-    LoadShader("src/shaders/vSpriteShader.glsl", "src/shaders/fSpriteShader.glsl", "SpriteShader");
-    LoadShader("src/shaders/vTextShader.glsl", "src/shaders/fTextShader.glsl", "TextShader");
-    
-	LoadTexture("GRASS_TILE_1", 1.0f, "sprites/grass_tile_1.png");
+    LoadShader(std::string(std::string(SHADER_PATH) + "vSpriteShader.glsl").c_str(), std::string(std::string(SHADER_PATH) + "fSpriteShader.glsl").c_str(), "SpriteShader");
+    LoadShader(std::string(std::string(SHADER_PATH) + "vTextShader.glsl").c_str(), std::string(std::string(SHADER_PATH) + "fTextShader.glsl").c_str(), "TextShader");
+
+    //LoadTexture("GRASS_TILE_1", 1.0f, "sprites/grass_tile_1.png");
     // Buildings
 
     // Fonts
-    LoadFont("Arial", "fonts/arial.ttf");
+    //LoadFont("Arial", "fonts/arial.ttf");
     return true;
 };
 
-const Shader &ResourceManager::GetShader(std::string name) {
+const Shader &ResourceManager::GetShader(std::string name)
+{
     return m_Shaders[name];
 }
 
-const Texture &ResourceManager::LoadTexture(std::string name, bool alpha, const char *file) {
+const Texture &ResourceManager::LoadTexture(std::string name, bool alpha, const char *file)
+{
     // create texture object
     Texture texture = Texture();
-    if (alpha) {
+    if(alpha)
+    {
         texture.Internal_Format = GL_RGBA;
         texture.Image_Format = GL_RGBA;
     }
@@ -53,30 +60,37 @@ const Texture &ResourceManager::LoadTexture(std::string name, bool alpha, const 
     return m_Textures[name];
 }
 
-const Texture &ResourceManager::GetTexture(std::string name) {
+const Texture &ResourceManager::GetTexture(std::string name)
+{
     return m_Textures[name];
 }
 
-const Font &ResourceManager::LoadFont(std::string name, std::string path) {
+const Font &ResourceManager::LoadFont(std::string name, std::string path)
+{
     FT_Library ft;
-    if (FT_Init_FreeType(&ft)) {
+    if(FT_Init_FreeType(&ft))
+    {
         std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
     }
 
     FT_Face face;
-    if (FT_New_Face(ft, path.c_str(), 0, &face)) {
+    if(FT_New_Face(ft, path.c_str(), 0, &face))
+    {
         std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
     }
     FT_Set_Pixel_Sizes(face, 0, 50);
-    if (FT_Load_Char(face, 'X', FT_LOAD_RENDER)) {
+    if(FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+    {
         std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
     }
 
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
 
-    for (unsigned char c = 0; c < 128; c++) {
+    for(unsigned char c = 0; c < 128; c++)
+    {
         // load character glyph
-        if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+        if(FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
             std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
             continue;
         }
@@ -102,15 +116,17 @@ const Font &ResourceManager::LoadFont(std::string name, std::string path) {
     return m_Fonts[name];
 }
 
-const Font &ResourceManager::GetFont(std::string name) {
+const Font &ResourceManager::GetFont(std::string name)
+{
     return m_Fonts[name];
 };
 
-void ResourceManager::Clear() {
+void ResourceManager::Clear()
+{
     // (properly) delete all shaders
-    for (auto iter : m_Shaders)
+    for(auto iter : m_Shaders)
         glDeleteProgram(iter.second.ID);
     // (properly) delete all textures
-    for (auto iter : m_Textures)
+    for(auto iter : m_Textures)
         glDeleteTextures(1, &iter.second.ID);
 }
